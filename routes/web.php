ec96dab67f63efbd870e778bc167c6ac0d041458
mabdelhameed420 <mabdelhameed420@gamil.com>
2023-04-10 17:45:24 +0200

    mo3

--- routes/web.php
+++ routes/web.php
@@ -75,3 +75,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('redirect/{services}', 'socialController@redirect');
 
 Route::get('callback/{services}', 'socialController@callback');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
